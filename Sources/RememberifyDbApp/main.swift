07e0f3bf91780be2a39e6a956016e176440c581d
Michael Pavkovic <mpavkovic688@gmail.com>
2021-01-24 22:26:00 -0500

    Code cleanup

--- Sources/RememberifyDbApp/main.swift
+++ Sources/RememberifyDbApp/main.swift
@@ -10,41 +10,39 @@ func main() {
         return
     }
 
-    print(spotifyAccessToken.accessToken)
-    let categoriesRequest = CategoriesRequest(token: spotifyAccessToken, country: "US", locale: "en_US")
+    let country = "US"
+    let categoriesRequest = CategoriesRequest(token: spotifyAccessToken, country: country, locale: "en_US")
 
-    if let categories = categoriesRequest.get() {
-        var playlists: [Playlist] = []
-        for category in categories.categories.items! {
-            let playlistsRequest = CategoryPlaylistsRequest(token: spotifyAccessToken,
-                categoryId: category.id, country: "SE")
+    guard let categories = categoriesRequest.get() else { return }
 
-            do {
-                try Coroutine.delay(.milliseconds(100))
-            } catch _ {}
-
-            if let playlistsInCategory = playlistsRequest.get(), let items = playlistsInCategory.playlists.items {
-                playlists.append(contentsOf: items)
-            } else {
-                print("Error getting category: \(category.id)")
-            }
-        }
-
-        print(playlists.count)
+    var playlists: [Playlist] = []
+    for category in categories.categories.items! {
+        let playlistsRequest = CategoryPlaylistsRequest(token: spotifyAccessToken,
+            categoryId: category.id, country: country)
 
         do {
-            let encoder = JSONEncoder()
-            encoder.keyEncodingStrategy = .convertToSnakeCase
-            encoder.outputFormatting = .prettyPrinted
+            try Coroutine.delay(.milliseconds(100))
+        } catch _ {}
 
-            if let data = try? encoder.encode(playlists) {
-                do {
-                    try data.write(to: FileManager.default.urls(for: .desktopDirectory,
-                        in: .userDomainMask)[0].appendingPathComponent("playlists.json"))
-                } catch let error {
-                    print(error)
-                }
-            }
+        if let playlistsInCategory = playlistsRequest.get(), let items = playlistsInCategory.playlists.items {
+            playlists.append(contentsOf: items)
+        } else {
+            print("Error getting category: \(category.id)")
+        }
+    }
+
+    print(playlists.count)
+
+    let encoder = JSONEncoder()
+    encoder.keyEncodingStrategy = .convertToSnakeCase
+    encoder.outputFormatting = .prettyPrinted
+
+    if let data = try? encoder.encode(playlists) {
+        do {
+            try data.write(to: FileManager.default.urls(for: .desktopDirectory,
+                in: .userDomainMask)[0].appendingPathComponent("playlists.json"))
+        } catch let error {
+            print(error)
         }
     }
 }
